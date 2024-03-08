d574af561628b74c6fcca0ceb961d3ebb776af28
Bastian <bjcplusplus@web.de>
2024-03-08 17:44:24 +0100

    fix: beautiful hair

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 def main():
   with open("books/frankenstein.txt") as f:
+    print("--- Begin report of books/frankenstein.txt ---")
     file_contents = f.read()
-    print(numberOfWords(file_contents))
-    print(countLetters(file_contents))
+    print(f"{numberOfWords(file_contents)} words found in the document")
+    list_of_letters = toList(countLetters(file_contents))
+    list_of_letters.sort(reverse=True, key=sort_on)
+    printListOfLetters(list_of_letters)
+    print("--- End report ---")
 
 def numberOfWords(txt):
   words = txt.split()
@@ -17,5 +21,20 @@ def countLetters(textFromBook):
     else:
       letterCounts[letter] += 1
   return letterCounts
+
+def toList(dict):
+  l = []
+  for letter in dict:    
+    td = {"letter":letter, "num": dict[letter]}
+    l.append(td)
+  return l
+
+def printListOfLetters(listOfLetters):
+  for letter in listOfLetters:
+    if letter['letter'].isalpha():
+      print(f"The '{letter['letter']}' character was found {letter['num']} times")
+
+def sort_on(dict):
+  return dict["num"]
 
 main()
